8772261a13712b4d6bcc17e593ff93b5768a2d16
Loli·小红帽 <1434052186@qq.com>
2016-05-12 18:45:46 +0800

    添加关于菜单的公共类。

---
+++ src/com/edu/zzti/yirongfinancial/common/MyMenu.java
@@ -0,0 +1,158 @@
+package com.edu.zzti.yirongfinancial.common;
+
+import com.edu.zzti.yirongfinancial.syw.R;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+
+public class MyMenu {
+
+	private Context context;
+	private WindowManager wm;
+	private LinearLayout yemian;
+	private ImageView caidan_button;
+	private RelativeLayout rl_caidan;
+	private ListView listview_caidan;
+
+	private int windowWidth;
+
+	private final int CAIDAN_OFF = 0;
+	private final int CAIDAN_ON = 1;
+	private int caidan = 0;
+
+	private LayoutParams layoutParams;
+
+	public MyMenu(Context context, WindowManager wm, LinearLayout yemian,
+			ImageView caidan_button, RelativeLayout rl_caidan,
+			ListView listview_caidan) {
+
+		this.context = context;
+		this.wm = wm;
+		this.yemian = yemian;
+		this.caidan_button = caidan_button;
+		this.rl_caidan = rl_caidan;
+		this.listview_caidan = listview_caidan;
+
+		listview_caidan.setAdapter(new MyAdapter());
+
+	}
+
+	public void myMenuPeiZhi() {
+
+		getWindowWidth();
+
+		layoutParams = new RelativeLayout.LayoutParams(windowWidth / 2,
+				LayoutParams.MATCH_PARENT);
+
+		layoutParams.setMargins(windowWidth, 0, 0, 0);
+
+		rl_caidan.setLayoutParams(layoutParams);
+
+		setOnClick();
+
+	}
+
+	private void getWindowWidth() {
+
+		windowWidth = wm.getDefaultDisplay().getWidth();
+
+	}
+
+	private void setOnClick() {
+
+		caidan_button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+
+				if (caidan == CAIDAN_OFF) {
+
+					yemian.scrollTo(windowWidth / 2, 0);
+
+					layoutParams.setMargins(windowWidth / 2, 0, 0, 0);
+
+					rl_caidan.setLayoutParams(layoutParams);
+
+					caidan = CAIDAN_ON;
+
+				} else if (caidan == CAIDAN_ON) {
+
+					yemian.scrollTo(0, 0);
+
+					layoutParams.setMargins(windowWidth, 0, 0, 0);
+
+					rl_caidan.setLayoutParams(layoutParams);
+
+					caidan = CAIDAN_OFF;
+
+				}
+
+			}
+		});
+
+	}
+
+	private class MyAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return 5;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+
+			View v = View.inflate(context, R.layout.wf_listview_item, null);
+
+			ImageView lv_im = (ImageView) v.findViewById(R.id.lv_im);
+			TextView lv_tv = (TextView) v.findViewById(R.id.lv_tv);
+
+			switch (arg0) {
+			case 0:
+				lv_im.setBackgroundResource(R.drawable.wf_guanyu);
+				lv_tv.setText("关于");
+				break;
+			case 1:
+				lv_im.setBackgroundResource(R.drawable.wf_jianceshengji);
+				lv_tv.setText("检测升级");
+				break;
+			case 2:
+				lv_im.setBackgroundResource(R.drawable.wf_fengxiang);
+				lv_tv.setText("分享");
+				break;
+			case 3:
+				lv_im.setBackgroundResource(R.drawable.wf_saoyisao);
+				lv_tv.setText("扫一扫");
+				break;
+			case 4:
+				lv_im.setBackgroundResource(R.drawable.wf_qinglihuancun);
+				lv_tv.setText("清理缓存");
+				break;
+
+			}
+
+			return v;
+		}
+	}
+
+}
--- src/com/edu/zzti/yirongfinancial/wf/Wf_main_activity.java
+++ src/com/edu/zzti/yirongfinancial/wf/Wf_main_activity.java
@@ -1,5 +1,6 @@
 package com.edu.zzti.yirongfinancial.wf;
 
+import com.edu.zzti.yirongfinancial.common.MyMenu;
 import com.edu.zzti.yirongfinancial.hwh.Hwh_fastcardaccess_activity;
 import com.edu.zzti.yirongfinancial.lkw.Lkw_credit_activity;
 import com.edu.zzti.yirongfinancial.syw.R;
@@ -13,7 +14,11 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class Wf_main_activity extends Activity {
@@ -23,13 +28,29 @@ public class Wf_main_activity extends Activity {
 	private LinearLayout ll_bankajindu;
 	private LinearLayout ll_wangluoxiaodai;
 	private LinearLayout ll_zhengxinchaxun;
+	private LinearLayout wf_main_yemian;
+	private ImageView wf_main_caidan_button;
+	private RelativeLayout wf_rl_caidan;
+	private ListView wf_listview_caidan;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.wf_main_activity);
+
 		findView();
 		setOnClick();
+		myMenu();
+
+	}
+
+	public void myMenu() {
+
+		MyMenu myMenu = new MyMenu(getApplicationContext(), getWindowManager(),
+				wf_main_yemian, wf_main_caidan_button, wf_rl_caidan,
+				wf_listview_caidan);
+
+		myMenu.myMenuPeiZhi();
 
 	}
 
@@ -63,6 +84,10 @@ public class Wf_main_activity extends Activity {
 		ll_bankajindu = (LinearLayout) findViewById(R.id.ll_bankajindu);
 		ll_wangluoxiaodai = (LinearLayout) findViewById(R.id.ll_wangluoxiaodai);
 		ll_zhengxinchaxun = (LinearLayout) findViewById(R.id.ll_zhengxinchaxun);
+		wf_main_yemian = (LinearLayout) findViewById(R.id.wf_main_yiemian);
+		wf_main_caidan_button = (ImageView) findViewById(R.id.wf_main_caidan_button);
+		wf_rl_caidan = (RelativeLayout) findViewById(R.id.wf_rl_caidan);
+		wf_listview_caidan = (ListView) findViewById(R.id.wf_listview_caidan);
 
 	}
 
