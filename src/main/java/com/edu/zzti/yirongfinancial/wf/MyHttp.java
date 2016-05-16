9b3b087f15874074ad0ec7fb459aa3c50c3f72bf
Loli·小红帽 <1434052186@qq.com>
2016-05-17 02:48:45 +0800

    框架完善，添加获取逻辑。

--- src/main/java/com/edu/zzti/yirongfinancial/wf/MyHttp.java
+++ src/main/java/com/edu/zzti/yirongfinancial/wf/MyHttp.java
@@ -1,57 +1,90 @@
 package com.edu.zzti.yirongfinancial.wf;
 
-import android.content.Context;
+import android.util.Xml;
 
-import com.edu.zzti.yirongfinancial.common.MyUtils;
+import org.xmlpull.v1.XmlPullParser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyHttp {
 
-    private static List<User> userList;
+    private static String downPath = "http://7xu2vf.com1.z0.glb.clouddn.com/users.xml";
+    private static String usersSavaPath = "data/data/com.edu.zzti.yirongfinancial.syw";
+    private static String usersSavaName = "users.xml";
+
+    private static String userSavaPath = "data/data/com.edu.zzti.yirongfinancial.syw";
+    private static String userSavaName = "user.xml";
 
     public static List<User> readUsers() {
 
-        userList = new ArrayList<User>();
-
-        User user1 = new User();
-
-        user1.setImei("000000000000000");
-        user1.setName("111");
-        user1.setPass(MyUtils.getMD5("222"));
-
-        userList.add(user1);
-
-        User user2 = new User();
-
-        user2.setImei("000000000000000");
-        user2.setName("222");
-        user2.setPass(MyUtils.getMD5("333"));
-
-        userList.add(user2);
-
-        User user3 = new User();
-
-        user3.setImei("000000000000000");
-        user3.setName("333");
-        user3.setPass(MyUtils.getMD5("444"));
-
-        userList.add(user3);
+        List<User> userList = new ArrayList<User>();
 
         return userList;
 
     }
 
-    public static boolean setUsers() {
+    private static List<User> readXML() {
+
+        List<User> userList = new ArrayList<User>();
+
+        return userList;
+
+    }
+
+    public static boolean saveUserSp() {
 
         return false;
 
     }
 
-    private static boolean getUsers() {
+    private static boolean downUsers() {
 
-        return true;
+        try {
+
+            URL url = new URL(downPath);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setConnectTimeout(3000);
+
+            if (conn.getResponseCode() == 200) {
+
+                InputStream in = conn.getInputStream();
+
+                File usersSavaFile = new File(usersSavaPath, usersSavaName);
+
+                FileOutputStream fos = new FileOutputStream(usersSavaFile);
+
+                int len = -1;
+                byte[] buffer = new byte[100];
+
+                while ((len = in.read(buffer)) != -1) {
+
+                    fos.write(buffer, 0, len);
+
+                }
+
+                fos.close();
+                in.close();
+
+                return true;
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        return false;
 
     }
 
