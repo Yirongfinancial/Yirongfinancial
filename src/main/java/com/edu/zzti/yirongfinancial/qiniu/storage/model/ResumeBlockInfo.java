5f2beb431b9b135207c951c76477b4ad90fabc10
syw123456 <2671523019@qq.com>
2016-05-18 13:13:23 +0800

    change gradle file

---
+++ src/main/java/com/edu/zzti/yirongfinancial/qiniu/storage/model/ResumeBlockInfo.java
@@ -0,0 +1,9 @@
+package com.edu.zzti.yirongfinancial.qiniu.storage.model;
+
+/**
+ * Created by bailong on 15/2/23.
+ */
+public class ResumeBlockInfo {
+    public String ctx;
+    public long crc32;
+}
